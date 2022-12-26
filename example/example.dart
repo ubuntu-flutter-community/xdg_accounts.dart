f7561c61060d77de695f7e49c96bcbf25fcfd8b0
Feichtmeier <frederik.feichtmeier@gmail.com>
2022-12-26 20:37:25 +0100

    update example

--- example/example.dart
+++ example/example.dart
@@ -1,22 +1,38 @@
 // ignore_for_file: avoid_print
 
+import 'dart:async';
+
 import 'package:linux_accounts_service/linux_accounts_service.dart';
+import 'package:linux_accounts_service/src/freedesktop_user.dart';
 
 Future<void> main() async {
   final service = LinuxAccountsService();
   await service.init();
 
-  await printAllUsers(service);
+  await printAllUsers(service.freeDesktopUsers);
+
+  List<StreamSubscription<FreeDesktopUserChanged>> subs = [];
+
+  for (var fu in service.freeDesktopUsers) {
+    final sub = fu.changed.listen((event) async {
+      // Process properties
+      print(fu.name);
+    });
+    subs.add(sub);
+  }
 
   // Change to an existing uid
-  // await service.changeUserName(uid: '1001', newUserName: 'anewname');
-  // await printAllUsers(service);
+  // await service.changeUserName(uid: '1001', newUserName: 'test');
+  // await printAllUsers(service.freeDesktopUsers);
 
+  for (var sub in subs) {
+    await sub.cancel();
+  }
   await service.dispose();
 }
 
-Future<void> printAllUsers(LinuxAccountsService service) async {
-  for (var fu in service.freeDesktopUsers) {
+Future<void> printAllUsers(List<FreeDesktopUser> freeDesktopUsers) async {
+  for (var fu in freeDesktopUsers) {
     var username = await fu.getUserName();
     var freeDesktopAccountType = await fu.getAccountType();
     var uid = await fu.getUid();
