4f14cf34f2168eb0946b631657c16b107882c165
Feichtmeier <frederik.feichtmeier@gmail.com>
2022-12-29 23:59:17 +0100

    Update all other props in getters and streams

--- lib/src/xdg_user.dart
+++ lib/src/xdg_user.dart
@@ -26,6 +26,31 @@ extension _XdgUserPropertiesChangedSignal on DBusPropertiesChangedSignal {
   bool get shellChanged => changedProperties.containsKey('Shell');
   bool get emailChanged => changedProperties.containsKey('Email');
   bool get languageChanged => changedProperties.containsKey('Language');
+  bool get sessionChanged => changedProperties.containsKey('Session');
+  bool get sessionTypeChanged => changedProperties.containsKey('SessionType');
+  bool get formatsLocaleChanged =>
+      changedProperties.containsKey('FormatsLocale');
+  bool get inputSourcesChanged => changedProperties.containsKey('InputSources');
+  bool get xSessionChanged => changedProperties.containsKey('XSession');
+  bool get locationChanged => changedProperties.containsKey('Location');
+  bool get loginFrequencyChanged =>
+      changedProperties.containsKey('LoginFrequency');
+  bool get loginTimeChanged => changedProperties.containsKey('LoginTime');
+  bool get xHasMessagesChanged => changedProperties.containsKey('XHasMessages');
+  bool get xKeyboardLayoutsChanged =>
+      changedProperties.containsKey('XKeyboardLayouts');
+  bool get backgroundFileChanged =>
+      changedProperties.containsKey('BackgroundFile');
+  bool get iconFileChanged => changedProperties.containsKey('IconFile');
+  bool get savedChanged => changedProperties.containsKey('Saved');
+  bool get lockedChanged => changedProperties.containsKey('Locked');
+  bool get passwordModeChanged => changedProperties.containsKey('PasswordMode');
+  bool get passwordHintChanged => changedProperties.containsKey('PasswordHint');
+  bool get automaticLoginChanged =>
+      changedProperties.containsKey('AutomaticLogin');
+  bool get systemAccountChanged =>
+      changedProperties.containsKey('SystemAccount');
+  bool get localAccountChanged => changedProperties.containsKey('LocalAccount');
 }
 
 class XdgUser extends DBusRemoteObject {
@@ -54,6 +79,24 @@ class XdgUser extends DBusRemoteObject {
     _shell = await getShell();
     _email = await getEmail();
     _language = await getLanguage();
+    _session = await getSession();
+    _sessionType = await getSessionType();
+    _formatsLocale = await getFormatsLocale();
+    _inputSources = await getInputSources();
+    _xSession = await getXSession();
+    _location = await getLocation();
+    _loginFrequency = await getLoginFrequency();
+    _loginTime = await getLoginTime();
+    _xHasMessages = await getXHasMessages();
+    _xKeyboardLayouts = await getXKeyboardLayouts();
+    _backgroundFile = await getBackgroundFile();
+    _saved = await getSaved();
+    _locked = await getLocked();
+    _passwordMode = await getPasswordMode();
+    _passwordHint = await getPasswordHint();
+    _automaticLogin = await getAutomaticLogin();
+    _systemAccount = await getSystemAccount();
+    _localAccount = await getLocalAccount();
     _propertyListener ??= propertiesChanged.listen(_updateProperties);
   }
 
@@ -78,6 +121,64 @@ class XdgUser extends DBusRemoteObject {
     }
     if (signal.languageChanged) {
       getLanguage().then(_updateLanguage);
+    }
+    if (signal.sessionChanged) {
+      getSession().then(_updateSession);
+    }
+    if (signal.sessionTypeChanged) {
+      getSessionType().then(_updateSessionType);
+    }
+    if (signal.formatsLocaleChanged) {
+      getFormatsLocale().then(_updateFormatsLocale);
+    }
+    if (signal.inputSourcesChanged) {
+      getInputSources().then(_updateInputSources);
+    }
+    if (signal.xSessionChanged) {
+      getXSession().then(_updateXSession);
+    }
+    if (signal.locationChanged) {
+      getLocation().then(_updateLocation);
+    }
+    if (signal.loginFrequencyChanged) {
+      getLoginFrequency().then(_updateLoginFrequency);
+    }
+    if (signal.loginTimeChanged) {
+      getLoginTime().then(_updateLoginTime);
+    }
+    if (signal.xHasMessagesChanged) {
+      getXHasMessages().then(_updateXHasMessages);
+    }
+    if (signal.xKeyboardLayoutsChanged) {
+      getXKeyboardLayouts().then(_updateXKeyboardLayouts);
+    }
+    if (signal.backgroundFileChanged) {
+      getBackgroundFile().then(_updateBackgroundFile);
+    }
+    if (signal.iconFileChanged) {
+      getIconFile().then(_updateIconFile);
+    }
+
+    if (signal.savedChanged) {
+      getSaved().then(_updateSaved);
+    }
+    if (signal.lockedChanged) {
+      getLocked().then(_updateLocked);
+    }
+    if (signal.passwordModeChanged) {
+      getPasswordMode().then(_updatePasswordMode);
+    }
+    if (signal.passwordHintChanged) {
+      getPasswordHint().then(_updatePasswordHint);
+    }
+    if (signal.automaticLoginChanged) {
+      getAutomaticLogin().then(_updateAutomaticLogin);
+    }
+    if (signal.systemAccountChanged) {
+      getSystemAccount().then(_updateSystemAccount);
+    }
+    if (signal.localAccountChanged) {
+      getLocalAccount().then(_updateLocalAccount);
     }
   }
 
@@ -250,6 +351,17 @@ class XdgUser extends DBusRemoteObject {
     return value.asString();
   }
 
+  // Session
+  final _sessionChangedController = StreamController<String>.broadcast();
+  Stream<String> get sessionChanged => _sessionChangedController.stream;
+  String? _session;
+  String? get session => _session;
+  void _updateSession(String? value) {
+    if (value == null) return;
+    _session = value;
+    _sessionChangedController.add(value);
+  }
+
   /// Gets org.freedesktop.Accounts.User.Session
   Future<String> getSession() async {
     var value = await getProperty(
@@ -258,6 +370,17 @@ class XdgUser extends DBusRemoteObject {
       signature: DBusSignature('s'),
     );
     return value.asString();
+  }
+
+  // SessionType
+  final _sessionTypeChangedController = StreamController<String>.broadcast();
+  Stream<String> get sessionTypeChanged => _sessionTypeChangedController.stream;
+  String? _sessionType;
+  String? get sessionType => _sessionType;
+  void _updateSessionType(String? value) {
+    if (value == null) return;
+    _sessionType = value;
+    _sessionTypeChangedController.add(value);
   }
 
   /// Gets org.freedesktop.Accounts.User.SessionType
@@ -270,6 +393,18 @@ class XdgUser extends DBusRemoteObject {
     return value.asString();
   }
 
+  // FormatsLocale
+  final _formatsLocaleChangedController = StreamController<String>.broadcast();
+  Stream<String> get formatsLocaleChanged =>
+      _formatsLocaleChangedController.stream;
+  String? _formatsLocale;
+  String? get formatsLocale => _formatsLocale;
+  void _updateFormatsLocale(String? value) {
+    if (value == null) return;
+    _formatsLocale = value;
+    _formatsLocaleChangedController.add(value);
+  }
+
   /// Gets org.freedesktop.Accounts.User.FormatsLocale
   Future<String> getFormatsLocale() async {
     var value = await getProperty(
@@ -278,6 +413,19 @@ class XdgUser extends DBusRemoteObject {
       signature: DBusSignature('s'),
     );
     return value.asString();
+  }
+
+  // InputSources
+  final _inputSourcesChangedController =
+      StreamController<List<Map<String, String>>>.broadcast();
+  Stream<List<Map<String, String>>> get inputSourcesChanged =>
+      _inputSourcesChangedController.stream;
+  List<Map<String, String>>? _inputSources;
+  List<Map<String, String>>? get inputSources => _inputSources;
+  void _updateInputSources(List<Map<String, String>>? value) {
+    if (value == null) return;
+    _inputSources = value;
+    _inputSourcesChangedController.add(value);
   }
 
   /// Gets org.freedesktop.Accounts.User.InputSources
@@ -297,6 +445,17 @@ class XdgUser extends DBusRemoteObject {
         .toList();
   }
 
+  // XSession
+  final _xSessionChangedController = StreamController<String>.broadcast();
+  Stream<String> get xSessionChanged => _xSessionChangedController.stream;
+  String? _xSession;
+  String? get xSession => _xSession;
+  void _updateXSession(String? value) {
+    if (value == null) return;
+    _xSession = value;
+    _xSessionChangedController.add(value);
+  }
+
   /// Gets org.freedesktop.Accounts.User.XSession
   Future<String> getXSession() async {
     var value = await getProperty(
@@ -305,6 +464,17 @@ class XdgUser extends DBusRemoteObject {
       signature: DBusSignature('s'),
     );
     return value.asString();
+  }
+
+  // Location
+  final _locationChangedController = StreamController<String>.broadcast();
+  Stream<String> get locationChanged => _locationChangedController.stream;
+  String? _location;
+  String? get location => _location;
+  void _updateLocation(String? value) {
+    if (value == null) return;
+    _location = value;
+    _locationChangedController.add(value);
   }
 
   /// Gets org.freedesktop.Accounts.User.Location
@@ -317,6 +487,18 @@ class XdgUser extends DBusRemoteObject {
     return value.asString();
   }
 
+  // LoginFrequency
+  final _loginFrequencyChangedController = StreamController<int>.broadcast();
+  Stream<int> get loginFrequencyChanged =>
+      _loginFrequencyChangedController.stream;
+  int? _loginFrequency;
+  int? get loginFrequency => _loginFrequency;
+  void _updateLoginFrequency(int? value) {
+    if (value == null) return;
+    _loginFrequency = value;
+    _loginFrequencyChangedController.add(value);
+  }
+
   /// Gets org.freedesktop.Accounts.User.LoginFrequency
   Future<int> getLoginFrequency() async {
     var value = await getProperty(
@@ -325,6 +507,17 @@ class XdgUser extends DBusRemoteObject {
       signature: DBusSignature('t'),
     );
     return value.asUint64();
+  }
+
+  // LoginTime
+  final _loginTimeChangedController = StreamController<int>.broadcast();
+  Stream<int> get loginTimeChanged => _loginTimeChangedController.stream;
+  int? _loginTime;
+  int? get loginTime => _loginTime;
+  void _updateLoginTime(int? value) {
+    if (value == null) return;
+    _loginTime = value;
+    _loginTimeChangedController.add(value);
   }
 
   /// Gets org.freedesktop.Accounts.User.LoginTime
@@ -345,6 +538,17 @@ class XdgUser extends DBusRemoteObject {
   //   return value.asArray().map((child) => child.asStruct()).toList();
   // }
 
+  // XHasMessages
+  final _xHasMessagesChangedController = StreamController<bool>.broadcast();
+  Stream<bool> get xHasMessagesChanged => _xHasMessagesChangedController.stream;
+  bool? _xHasMessages;
+  bool? get xHasMessages => _xHasMessages;
+  void _updateXHasMessages(bool? value) {
+    if (value == null) return;
+    _xHasMessages = value;
+    _xHasMessagesChangedController.add(value);
+  }
+
   /// Gets org.freedesktop.Accounts.User.XHasMessages
   Future<bool> getXHasMessages() async {
     var value = await getProperty(
@@ -353,6 +557,19 @@ class XdgUser extends DBusRemoteObject {
       signature: DBusSignature('b'),
     );
     return value.asBoolean();
+  }
+
+  // XKeyboardLayouts
+  final _xKeyboardLayoutsChangedController =
+      StreamController<List<String>>.broadcast();
+  Stream<List<String>> get xKeyboardLayoutsChanged =>
+      _xKeyboardLayoutsChangedController.stream;
+  List<String>? _xKeyboardLayouts;
+  List<String>? get xKeyboardLayouts => _xKeyboardLayouts;
+  void _updateXKeyboardLayouts(List<String>? value) {
+    if (value == null) return;
+    _xKeyboardLayouts = value;
+    _xKeyboardLayoutsChangedController.add(value);
   }
 
   /// Gets org.freedesktop.Accounts.User.XKeyboardLayouts
@@ -365,6 +582,18 @@ class XdgUser extends DBusRemoteObject {
     return value.asStringArray().toList();
   }
 
+  // BackgroundFile
+  final _backgroundFileChangedController = StreamController<String>.broadcast();
+  Stream<String> get backgroundFileChanged =>
+      _backgroundFileChangedController.stream;
+  String? _backgroundFile;
+  String? get backgroundFile => _backgroundFile;
+  void _updateBackgroundFile(String? value) {
+    if (value == null) return;
+    _backgroundFile = value;
+    _backgroundFileChangedController.add(value);
+  }
+
   /// Gets org.freedesktop.Accounts.User.BackgroundFile
   Future<String> getBackgroundFile() async {
     var value = await getProperty(
@@ -373,6 +602,17 @@ class XdgUser extends DBusRemoteObject {
       signature: DBusSignature('s'),
     );
     return value.asString();
+  }
+
+  // IconFile
+  final _iconFileChangedController = StreamController<String>.broadcast();
+  Stream<String> get iconFileChanged => _iconFileChangedController.stream;
+  String? _iconFile;
+  String? get iconFile => _iconFile;
+  void _updateIconFile(String? value) {
+    if (value == null) return;
+    _iconFile = value;
+    _iconFileChangedController.add(value);
   }
 
   /// Gets org.freedesktop.Accounts.User.IconFile
@@ -385,6 +625,17 @@ class XdgUser extends DBusRemoteObject {
     return value.asString();
   }
 
+  // Saved
+  final _savedChangedController = StreamController<bool>.broadcast();
+  Stream<bool> get savedChanged => _savedChangedController.stream;
+  bool? _saved;
+  bool? get saved => _saved;
+  void _updateSaved(bool? value) {
+    if (value == null) return;
+    _saved = value;
+    _savedChangedController.add(value);
+  }
+
   /// Gets org.freedesktop.Accounts.User.Saved
   Future<bool> getSaved() async {
     var value = await getProperty(
@@ -393,6 +644,17 @@ class XdgUser extends DBusRemoteObject {
       signature: DBusSignature('b'),
     );
     return value.asBoolean();
+  }
+
+  // Locked
+  final _lockedChangedController = StreamController<bool>.broadcast();
+  Stream<bool> get lockedChanged => _lockedChangedController.stream;
+  bool? _locked;
+  bool? get locked => _locked;
+  void _updateLocked(bool? value) {
+    if (value == null) return;
+    _locked = value;
+    _lockedChangedController.add(value);
   }
 
   /// Gets org.freedesktop.Accounts.User.Locked
@@ -405,6 +667,17 @@ class XdgUser extends DBusRemoteObject {
     return value.asBoolean();
   }
 
+  // PasswordMode
+  final _passwordModeChangedController = StreamController<int>.broadcast();
+  Stream<int> get passwordModeChanged => _passwordModeChangedController.stream;
+  int? _passwordMode;
+  int? get passwordMode => _passwordMode;
+  void _updatePasswordMode(int? value) {
+    if (value == null) return;
+    _passwordMode = value;
+    _passwordModeChangedController.add(value);
+  }
+
   /// Gets org.freedesktop.Accounts.User.PasswordMode
   Future<int> getPasswordMode() async {
     var value = await getProperty(
@@ -413,6 +686,18 @@ class XdgUser extends DBusRemoteObject {
       signature: DBusSignature('i'),
     );
     return value.asInt32();
+  }
+
+  // PasswordHint
+  final _passwordHintChangedController = StreamController<String>.broadcast();
+  Stream<String> get passwordHintChanged =>
+      _passwordHintChangedController.stream;
+  String? _passwordHint;
+  String? get passwordHint => _passwordHint;
+  void _updatePasswordHint(String? value) {
+    if (value == null) return;
+    _passwordHint = value;
+    _passwordHintChangedController.add(value);
   }
 
   /// Gets org.freedesktop.Accounts.User.PasswordHint
@@ -425,6 +710,18 @@ class XdgUser extends DBusRemoteObject {
     return value.asString();
   }
 
+  // AutomaticLogin
+  final _automaticLoginChangedController = StreamController<bool>.broadcast();
+  Stream<bool> get automaticLoginChanged =>
+      _automaticLoginChangedController.stream;
+  bool? _automaticLogin;
+  bool? get automaticLogin => _automaticLogin;
+  void _updateAutomaticLogin(bool? value) {
+    if (value == null) return;
+    _automaticLogin = value;
+    _automaticLoginChangedController.add(value);
+  }
+
   /// Gets org.freedesktop.Accounts.User.AutomaticLogin
   Future<bool> getAutomaticLogin() async {
     var value = await getProperty(
@@ -435,6 +732,18 @@ class XdgUser extends DBusRemoteObject {
     return value.asBoolean();
   }
 
+  // SystemAccount
+  final _systemAccountChangedController = StreamController<bool>.broadcast();
+  Stream<bool> get systemAccountChanged =>
+      _systemAccountChangedController.stream;
+  bool? _systemAccount;
+  bool? get systemAccount => _systemAccount;
+  void _updateSystemAccount(bool? value) {
+    if (value == null) return;
+    _systemAccount = value;
+    _systemAccountChangedController.add(value);
+  }
+
   /// Gets org.freedesktop.Accounts.User.SystemAccount
   Future<bool> getSystemAccount() async {
     var value = await getProperty(
@@ -443,6 +752,16 @@ class XdgUser extends DBusRemoteObject {
       signature: DBusSignature('b'),
     );
     return value.asBoolean();
+  }
+
+  final _localAccountChangedController = StreamController<bool>.broadcast();
+  Stream<bool> get localAccountChanged => _localAccountChangedController.stream;
+  bool? _localAccount;
+  bool? get localAccount => _localAccount;
+  void _updateLocalAccount(bool? value) {
+    if (value == null) return;
+    _localAccount = value;
+    _localAccountChangedController.add(value);
   }
 
   /// Gets org.freedesktop.Accounts.User.LocalAccount
